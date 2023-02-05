43649156b696a24a5c54aadb2c1750821fc60fad
davi <davijluna@gmail.com>
2023-02-05 02:47:04 -0300

    feat:requisito 1 implementando pacote ting_file_management

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,18 +1,25 @@
 from ting_file_management.abstract_queue import AbstractQueue
+# import threading, queue
+# fila = queue.Queue()
 
 
 class Queue(AbstractQueue):
+
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+        self.queue = list()
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return len(self.queue)
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        self.queue.append(value)
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        if len(self.queue) == 0:
+            raise IndexError('erro em dequene')
+        return self.queue.pop(0)
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        if not 0 <= index < len(self.queue):
+            raise IndexError('não foi encontrado')
+        return self.queue[index]

77234d67bbb1d96dd7e934df15997903b8bdd0ab
davi <davijluna@gmail.com>
2023-02-07 13:15:03 -0300

    feat: requisito 2 implementando função txt_importer

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,13 @@
+import sys
+import pathlib
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if pathlib.Path(path_file).suffix != '.txt':
+        return sys.stderr.write(f"{'Formato inválido'}")
+    try:
+        with open(path_file, "r") as arquivo:
+            txt_list = arquivo.read().split("\n")
+            return txt_list
+    except FileNotFoundError:
+        return sys.stderr.write(f'Arquivo {path_file} não encontrado\n')

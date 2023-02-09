14ad3faf3d252f6495fac966bd3507fe1e253c9d
davi <davijluna@gmail.com>
2023-02-09 12:03:41 -0300

    feat: requidito 3  implementando função process

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,26 @@
-def process(path_file, instance):
+from ting_file_management.file_management import txt_importer
+from ting_file_management.queue import Queue
+import sys
+
+
+def process(path_file, instance: Queue):
     """Aqui irá sua implementação"""
+    # dict = {}
+    lista = txt_importer(path_file)
+    for i in range(instance.__len__()):
+        dict = instance.search(i)
+        if dict["nome_do_arquivo"] == path_file:
+            return
+
+    dict = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(lista),
+        "linhas_do_arquivo": lista,
+    }
+
+    instance.enqueue(dict)
+    sys.stdout.write(str(dict))
+    # print(dict, file=sys.stdout)
 
 
 def remove(instance):
@@ -8,3 +29,6 @@ def remove(instance):
 
 def file_metadata(instance, position):
     """Aqui irá sua implementação"""
+
+
+# print(process(txt_importer()))

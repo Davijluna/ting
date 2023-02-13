d620de6847322c92e8e02a682c0b2693e08b336f
davi <davijluna@gmail.com>
2023-02-13 12:01:10 -0300

    requisito 7 implementando a função exists_word obs não esta passando um teste mas comitei para ver se passa no github

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,25 @@
-def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+# import os
+from ting_file_management.queue import Queue
+
+
+def exists_word(word, instance: Queue):
+    result = []
+    for index in range(len(instance)):
+        test = instance.search(index)
+        list = {
+            "palavra": word,
+            "arquivo": test["nome_do_arquivo"],
+            "ocorrencias": []
+        }
+        for index2, text in enumerate(test["linhas_do_arquivo"]):
+            if word.casefold() in text.casefold():
+                list["ocorrencias"].append({"linha": index2 + 1})
+        if len(list["ocorrencias"]):
+            result.append(
+                list["palavra"],
+                list["arquivo"],
+                list["ocorrencias"])
+    return result
 
 
 def search_by_word(word, instance):
